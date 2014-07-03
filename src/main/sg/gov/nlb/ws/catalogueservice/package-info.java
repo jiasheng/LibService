ae6f155b0107940af6b09df8d3da9081b1cd285b
jiasheng <fongjiasheng@gmail.com>
2014-07-03 19:45:54 +0800

    try1

--- src/main/sg/gov/nlb/ws/catalogueservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.nlb.gov.sg/ws/CatalogueService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package sg.gov.nlb.ws.catalogueservice;
